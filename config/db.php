3e018362051780068dff2bf16d3f0b7140f0a3ca
vaishnaviramesh <vaishnaviramesh956@gmail.com>
2026-06-09 08:35:39 +0530

    Fixed localhost db connection

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
-define('DB_HOST', 'sql208.byetcluster.com');
-define('DB_USER', 'b31_42119499');
-define('DB_PASS', 'hotel1@2026');
-define('DB_NAME', 'b31_42119499_hotel');
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "hotel_food_db";
 
-$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = mysqli_connect($hostname, $username, $password, $database);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
